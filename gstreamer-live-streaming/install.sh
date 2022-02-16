0b43508c7c63a414b2fe86613a46b070dc748928
Laura Mazzuca <lauramazzuca21@icloud.com>
2022-02-16 20:17:45 +0100

    adds Dockerfile for webRTC streamer ; adds vscode environment setup ; adds gstreamer cmake find module

--- gstreamer-live-streaming/install.sh
+++ gstreamer-live-streaming/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -O0 -ggdb -Wall -fno-omit-frame-pointer media-live-streamer.c -o ./bin/mls `pkg-config --cflags --libs gstreamer-webrtc-1.0 gstreamer-sdp-1.0 libsoup-2.4 json-glib-1.0`
+gcc -O0 -ggdb -Wall -fno-omit-frame-pointer webRTC-streamer.c -o main `pkg-config --cflags --libs gstreamer-webrtc-1.0 gstreamer-sdp-1.0 libsoup-2.4 json-glib-1.0`
